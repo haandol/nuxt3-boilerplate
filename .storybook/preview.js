a9a48d0789794c9b472b690f6e74b21656daf342
haandol <ldg55d@gmail.com>
2022-06-21 15:50:53 +0900

    apply alias and fix storybooks

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,5 +1,15 @@
+import '../assets/css/tailwind.css'
+import { app } from '@storybook/vue3'
+import { createPinia } from 'pinia'
+import { INITIAL_VIEWPORTS } from '@storybook/addon-viewport'
+
+app.use(createPinia())
+
 export const parameters = {
-  actions: { argTypesRegex: "^on[A-Z].*" },
+  actions: { argTypesRegex: '^on[A-Z].*' },
+  viewport: {
+    viewports: INITIAL_VIEWPORTS,
+  },
   controls: {
     matchers: {
       color: /(background|color)$/i,
---
+++ vite.config.ts
@@ -0,0 +1,13 @@
+import path from 'path'
+import { defineConfig } from 'vite'
+import eslintPlugin from 'vite-plugin-eslint'
+
+export default defineConfig({
+  plugins: [eslintPlugin()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname),
+      '@assets': path.resolve(__dirname, 'assets'),
+    },
+  },
+})
